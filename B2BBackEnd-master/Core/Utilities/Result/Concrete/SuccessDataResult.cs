044d0c8464dfc77492309fb2f40483fa8fe35389
ali kahveci <126388262+alikahveci-m@users.noreply.github.com>
2023-09-01 18:09:06 +0300

    b2b-frontend-needed

---
+++ B2BBackEnd-master/Core/Utilities/Result/Concrete/SuccessDataResult.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Dtos;
+
+namespace Core.Utilities.Result.Concrete
+{
+    public class SuccessDataResult<T> : DataResult<T>
+    {
+        private List<ProductListDto> productListDtos;
+
+        public SuccessDataResult(T data) : base(data, true)
+        {
+        }
+
+        public SuccessDataResult(T data, string message) : base(data, true, message)
+        {
+        }
+
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+        }
+    }
+}
